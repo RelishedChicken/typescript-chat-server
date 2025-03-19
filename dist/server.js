b0874b9690629bec45e8e1a40fbec40f4c6c42f4
RelishedChicken <veerajvadgama@gmail.com>
2025-03-19 17:48:12 +0000

    setup basic node server for typescript-chat

---
+++ dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = require("socket.io");
+const io = new socket_io_1.Server(3000, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+let messages = [];
+//Handle a client connection
+io.on("connection", (socket) => {
+    //Handle sending the client all messages currently stored
+    socket.on('getAllMessages', () => {
+        socket.emit("sendMessages", messages);
+    });
+    //Handle recieving a message
+    socket.on('message', (message) => {
+        console.log("Recieved: " + message.message + "[" + message.id + "]");
+        messages.push(message);
+        socket.emit("sendMessages", messages);
+    });
+});
---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import {Server} from 'socket.io';
+
+interface ServerToClientEvents {
+    sendMessages: (messages:ChatMessage[]) => void;
+}
+
+interface ClientToServerEvents {
+    message: (message:ChatMessage) => void;
+    getAllMessages: () => void;
+}
+
+interface SocketData {
+    name: string;
+    age: number;
+}
+
+interface ChatMessage {
+    readonly id: number;
+    readonly message: string;
+    readonly date: Date;
+}
+
+const io = new Server<ClientToServerEvents,ServerToClientEvents,SocketData>(3000 , {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+let messages:ChatMessage[] = [];
+
+//Handle a client connection
+io.on("connection", (socket) => {
+
+    //Handle sending the client all messages currently stored
+    socket.on('getAllMessages', () => {
+        socket.emit("sendMessages", messages);
+    });
+
+    //Handle recieving a message
+    socket.on('message', (message:ChatMessage) => {
+        console.log("Recieved: " + message.message + "["+message.id+"]");
+        messages.push(message);
+        socket.emit("sendMessages", messages);
+    });
+
+});
+
