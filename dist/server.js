41077321968158becbc7febf4191f3c8c672493d
RelishedChicken <veerajvadgama@gmail.com>
2025-03-21 17:00:58 +0000

    updated index to be in dist

--- dist/server.js
+++ dist/server.js
@@ -2,19 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const socket_io_1 = require("socket.io");
 const express_1 = __importDefault(require("express"));
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const app = (0, express_1.default)();
-const development = 'http://localhost';
-const url = ((_a = process.env.NODE_ENV) !== null && _a !== void 0 ? _a : development);
+const url = (process.env.NODE_ENV) ? 'https://typescript-chat-server-2a4af974e68f.herokuapp.com' : 'http://localhost';
 const server = app.use((req, res) => res.sendFile(INDEX, { root: __dirname })).listen(PORT, () => console.log(`Listening on ${url}:${PORT}`));
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true
